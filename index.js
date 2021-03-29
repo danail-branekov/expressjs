a5c3ab63e4923511bc586dd036705c43c39fccbf
Danail Branekov <danailster@gmail.com>
2021-03-29 12:57:51 +0000

    Test commit
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Have a nice day!')
+  res.send('Hello World! Have a nice evening!')
 })
 
 app.listen(port, () => {
